32908b8ee8515bc0bee98c5520e2b9867542ab55
Fujimura Daisuke <me@fujimuradaisuke.com>
2014-06-10 17:15:07 +0900

    Add first test

--- test/Git/FreqSpec.hs
+++ test/Git/FreqSpec.hs
@@ -1,8 +1,39 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Git.FreqSpec ( spec ) where
 
+import           Control.Applicative
+import           Data.IORef
+import qualified Data.Text           as T
 import           Git.Freq
+import           Git.Freq.Source
 import           Test.Hspec
+
+type MockSource = IORef [String]
+
+instance Source MockSource where
+    isEOF s = null <$> readIORef s
+    getLine s = readIORef s >>= go
+      where
+        go [] = error "Empty"
+        go (x:xs) = do
+          writeIORef s xs
+          return $ T.pack x
+
+createMockSource :: [String] -> IO MockSource
+createMockSource = newIORef
 
 spec :: Spec
 spec = do
-    it "should be ok" $ True `shouldBe` True
+    describe "freq'" $
+      it "should summarize changes" $ do
+        source <- createMockSource [ "1\t1\tgit-freq.cabal"
+                                   , "2\t5\tREADME.md"
+                                   , "3\t0\tgit-freq.cabal"
+                                   , "2\t9\tREADME.md"
+                                   ]
+        freq' source `shouldReturn` [ ("README.md", (4, 14))
+                                    , ("git-freq.cabal", (4, 1))
+                                    ]
